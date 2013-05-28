c3ed88d2af5e0aa103dccbda6f08c4311f6f1279
Iwe Muiser <e.c.muiser@gmail.com>
2013-05-28 10:42:29 +0200

    typo

--- VerhalenbankFunctionsPlugin.php
+++ VerhalenbankFunctionsPlugin.php
@@ -526,7 +526,7 @@ De inhoud is daarom afgeschermd, en kan alleen worden geraadpleegd op het Meerte
         $folktale_html .= "<H2>Invoerhulp websites / lijsten</H2><br>";
         $folktale_html .= '<UL STYLE="list-style-type: disc;">';
         $folktale_html .= "<li><a target='manual' href='http://bookstore.ewi.utwente.nl/docs/Handleiding%20Nieuwe%20Volksverhalenbank%20Versie%202.pdf'><b>Handleiding</b> invoer Nederlandse Volksverhalenbank</a><br>";
-        $folktale_html .= "<li><a target='motieven' href='http://www.dinor.demon.nl/Thompson/'>Browse/zoek <b>Thompson movieven</b> (website Dirk Kramer)</a><br>";
+        $folktale_html .= "<li><a target='motieven' href='http://www.dinor.demon.nl/Thompson/'>Browse/zoek <b>Thompson motieven</b> (website Dirk Kramer)</a><br>";
         $folktale_html .= "<li><a target='kloekenummers' href='http://www.meertens.knaw.nl/kloeke/'>Zoek <b>Kloeke nummers</b> (website Meertens)</a><br>";
         $folktale_html .= "</UL><br>";
 
