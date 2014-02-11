37fa5d48d8be1349fae672529e6d05168509b437
Iwe Muiser <e.c.muiser@gmail.com>
2014-02-11 13:27:15 +0100

    more autocomplete functions

--- views/shared/javascripts/input-autocompleter.js
+++ views/shared/javascripts/input-autocompleter.js
@@ -1,6 +1,15 @@
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds){
+      break;
+    }
+  }
+}
+
+
 // Parse an item and create an title/value hash table with all the properties available
 function getFields(results) {
-    console.log(results);
     if(results.value == undefined) {
 	    return results;
     }
@@ -9,69 +18,130 @@ function getFields(results) {
     }
 }
 
-function ajax_delay(str){
- setTimeout("str",2000);
+// Parse an item and create an title/value hash table with all the properties available
+function addIdValue(results) {
+    return results.value;
 }
 
-function autoautomulti(){
-    buttonrefresh();
-    jQuery("textarea[name*='Elements[52]']").each( function( index, el){
-        jQuery( el ).autocomplete({
-    		minLength: 1,
-    		source: function( request, response ) {
-    			jQuery.ajax({
-    			    beforeSend: function (request)
-    				{
-    					request.setRequestHeader("Accept", "application/json;odata=verbose;charset=utf-8");
-    				},
-    				url: "http://bookstore.ewi.utwente.nl/afact/motif.json?style=list&code=" + request.term, //NO CROSS SITE SCRIPTING YET
-//    				url: "http://127.0.0.1/testing/resting/motif.json?style=list&code=" + request.term, 
-    				dataType: "json",
-    				success: function( data ) {
-    					response( jQuery.map( data.data, function( item ) {
-    						return {
-                                fields: getFields(item)
-    						}
-    					}));
-    				},
-    				error: function( data ) {
-    				    return "None found";
-    				}
-    			});
-    		},
-    		focus: function( event, ui ) {
-                jQuery( el ).val( ui.item.fields.id );
-                return false;
-            },
-            // Run this when the item is selected
-            select: function( event, ui ) {
-                jQuery( el ).val( ui.item.fields.id );
-                return false;   
-    		},
-    		appendTo: jQuery('#menu-container')
-    	}).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
-    	    if (jQuery( el )[0].value.toUpperCase() == item.fields.id){
-    		    return jQuery("<li style='color:green'>").append("<a>" + item.fields.id + " - " + item.fields.title + "</a>")
-    			.appendTo( ul );
-    	    }
-    	    else{
-    		    return jQuery("<li>").append( "<a>" + item.fields.id + " - " + item.fields.title + "</a>")
-    			.appendTo( ul );
-    		}
-        };
+function autoMultiInternal(element, collection, url){
+}
+    
+
+function autoMultiAdd(element, url){
+    jQuery( element ).autocomplete({
+        minLength: 1,
+        source: function( request, response ) {
+            jQuery.ajax({
+                beforeSend: function (request)
+                {
+                    request.setRequestHeader("Accept", "application/json;odata=verbose;charset=utf-8");
+                },
+                url: url + request.term,
+                dataType: "json",
+                success: function( data ) {
+                    response( jQuery.map( data.data, function( item ) {
+                        return {
+                            fields: getFields(item)
+                        }
+                    }));
+                },
+                error: function( data ) {
+                    return "None found";
+                }
+            });
+        },
+        focus: function( event, ui ) {
+            jQuery( element ).val( ui.item.fields.id );
+            return false;
+        },
+        // Run this when the item is selected
+        select: function( event, ui ) {
+            jQuery( element ).val( ui.item.fields.id );
+            return false;   
+        },
+        appendTo: jQuery('#menu-container')
+    }).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+        return jQuery("<li>").append( "<p style='color:red'>" + item.fields.id + "</p>")
+        .appendTo( ul );
+    };
+};
+
+function autoMultiExternal(element, url){
+    jQuery( element ).autocomplete({
+		minLength: 1,
+		source: function( request, response ) {
+			jQuery.ajax({
+			    beforeSend: function (request)
+				{
+					request.setRequestHeader("Accept", "application/json;odata=verbose;charset=utf-8");
+				},
+				url: url + request.term,
+				dataType: "json",
+				success: function( data ) {
+					response( jQuery.map( data.data, function( item ) {
+						return {
+                            fields: getFields(item) 
+						}
+					}));
+				},
+				error: function( data ) {
+//				    console.log(data);
+				    return "None found";
+				}
+			});
+		},
+		focus: function( event, ui ) {
+            jQuery( element ).val( ui.item.fields.id );
+            return false;
+        },
+        // Run this when the item is selected
+        select: function( event, ui ) {
+            jQuery( element ).val( ui.item.fields.id );
+            return false;   
+		},
+		appendTo: jQuery('#menu-container')
+	}).data( "ui-autocomplete" )._renderItem = function( ul, item ) {
+        if (jQuery( element )[0].value.toUpperCase() == item.fields.id){
+            return jQuery("<li style='color:green'>").append("<a>" + item.fields.id + " - " + item.fields.title + "</a>")
+            .appendTo( ul );
+        }
+        else{
+            return jQuery("<li>").append( "<a>" + item.fields.id + " - " + item.fields.title + "</a>")
+            .appendTo( ul );
+        }
+    };
+};
+
+function autocompleteAdd(elementNumber, url){
+    jQuery(".container").on("click", "#element-" + elementNumber + " textarea", function () {    
+        jQuery("textarea[id^=Elements-" + elementNumber + "]").each( function( index, el){
+            autoMultiAdd(jQuery(el), url);
+        });
     });
 };
 
-
-function buttonrefresh(){
-    jQuery('.add-element').click(function(){
-        setTimeout(autoautomulti,400);
+function autocompleteExternal(elementNumber, url){
+    jQuery(".container").on("click", "#element-" + elementNumber + " textarea", function () {    
+        jQuery("textarea[id^=Elements-" + elementNumber + "]").each( function( index, el){
+            autoMultiExternal(jQuery(el), url);
+        });
     });
-    jQuery('#item-type').change(function(){
-        setTimeout(autoautomulti,1000);
+};
+
+function dateTranslator(elementNumber){
+    jQuery(".container").on("click", "#element-" + elementNumber + " textarea", function () {    
+        jQuery("textarea[id^=Elements-" + elementNumber + "]").each( function( index, el){
+            jQuery(el)._renderItem = function( div, item ) {
+                return jQuery("<li style='color:green'>").append("<p>" + item.fields.id + " - " + item.fields.title + "</p>")
+                .appendTo( div );
+            };
+        });
     });
 }
 
 jQuery(document).ready(function() {
-    buttonrefresh()
+    autocompleteAdd("43", "http://127.0.0.1/testing/resting/identifier.json?style=list&code="); //identificatie code helper
+    autocompleteExternal("52", "http://127.0.0.1/testing/resting/motif.json?style=list&code="); //motif helper
+    autocompleteExternal("49", "http://127.0.0.1/testing/resting/folktaletype.json?style=list&search="); //folktaletype helper
+    dateTranslator("40"); //date helper
 });
