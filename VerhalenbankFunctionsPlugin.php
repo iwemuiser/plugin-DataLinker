bff68264985c8a65d249ffd7bce27eb8b88520db
Iwe Muiser <e.c.muiser@gmail.com>
2014-10-03 15:04:27 +0200

    te veel fix

--- VerhalenbankFunctionsPlugin.php
+++ VerhalenbankFunctionsPlugin.php
@@ -281,7 +281,7 @@ De inhoud is daarom afgeschermd, en kan alleen worden geraadpleegd op het Meerte
     private function vertellerVerhalenVerteld($verteller){
         $maxverhalen = 50;
         $verteldeVerhalen = NULL;
-        $amount_tales = __(" (max. $maxverhalen resultaten)");
+        $amount_tales = __("max. $maxverhalen weergegeven");
         if (metadata("item", 'collection_name') == "Vertellers"){
             $verteldeVerhalen = get_list_elements_by_value($verteller, "Creator", 1, $maxverhalen);
 #            $amount_tales = count($verteldeVerhalen);
